ef866d2648c737eb0f9b9eb16c9a2ea12d3c5df6
footballjones <footballjones308@gmail.com>
2023-04-08 21:59:19 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(choice = 'roller-skate') {
+    return `This Saturday, I want to ${choice}!`
+}
+function mondayWork(choice = 'go to the office') {
+    return `This Monday, I will ${choice}.`
+}
+function wrapAdjective(scope = '*') {
+    return function(string = 'special') {
+        return `You are ${scope}${string}${scope}!`
+    }
+}
+wrapAdjective('||')('a dedicated programmer')
